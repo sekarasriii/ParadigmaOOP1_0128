d1cfd6559e668c3c017e85dbb5fa7671a89b0d82
sekarasriii <sekareeesh@gmail.com>
2024-05-20 11:23:48 +0700

    membuat constructor

--- Agregrasi/Ibu.h
+++ Agregrasi/Ibu.h
@@ -10,5 +10,14 @@ using namespace std;
 class Ibu {
 public:
 	string nama;
+	vector<Anak*> daftar_anak;
 
+	Ibu(string pNama) :nama(pNama) {
+		cout << "Ibu\"" << nama << "\" ada\n";
+	}
+	~Ibu() {
+		cout << "Ibu\"" << nama << "\" tidak ada\n";
+	}
+	void tambahAnak(Anak*);
+	void cetakAnak();
 };

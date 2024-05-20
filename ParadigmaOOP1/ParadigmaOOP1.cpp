ac56754117d0891f34b3747e41ef445054bb7424
sekarasriii <sekareeesh@gmail.com>
2024-05-20 11:10:23 +0700

    membuat fungsi main untuk composisi

--- ParadigmaOOP1/ParadigmaOOP1.cpp
+++ ParadigmaOOP1/ParadigmaOOP1.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+#include "KomposisiJantung.h"
+#include "KomposisiManusia.h"
+
+int main() {//composisi
+	Manusia* varManusia = new Manusia("Jono");
+	delete varManusia;
+	return 0;
+}

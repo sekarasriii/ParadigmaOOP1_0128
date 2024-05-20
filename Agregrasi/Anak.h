759ea779476783a0d11adc1a67f69541547fe8fc
sekarasriii <sekareeesh@gmail.com>
2024-05-20 11:18:16 +0700

    membuat perintah destructor

--- Agregrasi/Anak.h
+++ Agregrasi/Anak.h
@@ -6,13 +6,13 @@
 #include <string>
 using namespace std;
 
-class anak {
+class Anak {
 public:
 	string nama;
-	anak(string pNama) : nama(pNama) {
+	Anak(string pNama) : nama(pNama) {
 		cout << "Anak \"" << nama << "\" ada\n";
 	}
-	~anak() {
+	~Anak() {
 		cout << "Anak \"" << nama << "\" tidak ada\n";
 	}
 };

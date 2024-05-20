2967a6d4097dab0762e884269ee1add73bb8f106
sekarasriii <sekareeesh@gmail.com>
2024-05-20 11:44:16 +0700

    membuat fungsi main

--- Agregrasi/Agregrasi.cpp
+++ Agregrasi/Agregrasi.cpp
@@ -5,5 +5,25 @@ using namespace std;
 #include "Ibu.h"
 
 int main() {//agregrasi
+	Ibu* varIbu = new Ibu("Dini");
+	Ibu* varIbu2 = new Ibu("Novi");
+	Anak* varAnak1 = new Anak("Tono");
+	Anak* varAnak2 = new Anak("Rini");
+	Anak* varAnak3 = new Anak("Dewi");
 
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varIbu2->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak3);
+
+	varIbu->cetakAnak();
+	varIbu2->cetakAnak();
+
+	delete varIbu;
+	delete varIbu2;
+	delete varAnak1;
+	delete varAnak2;
+	delete varAnak3;
+	
+	return 0;
 }
